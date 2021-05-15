1392ea32e7500cf44305790a8d4178c5aadbe847
Marin <angelo@MYPC>
2021-05-15 13:08:55 +0200

    first commit

---
+++ WindowsService9v2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WindowsService9v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.bt_obrada = new System.Windows.Forms.Button();
+            this.bt_prekini = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(152, 177);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(436, 29);
+            this.progressBar.TabIndex = 0;
+            // 
+            // bt_obrada
+            // 
+            this.bt_obrada.Location = new System.Drawing.Point(171, 80);
+            this.bt_obrada.Name = "bt_obrada";
+            this.bt_obrada.Size = new System.Drawing.Size(101, 33);
+            this.bt_obrada.TabIndex = 1;
+            this.bt_obrada.Text = "Obrađuj";
+            this.bt_obrada.UseVisualStyleBackColor = true;
+            this.bt_obrada.Click += new System.EventHandler(this.bt_obrada_Click);
+            // 
+            // bt_prekini
+            // 
+            this.bt_prekini.Location = new System.Drawing.Point(484, 80);
+            this.bt_prekini.Name = "bt_prekini";
+            this.bt_prekini.Size = new System.Drawing.Size(104, 33);
+            this.bt_prekini.TabIndex = 2;
+            this.bt_prekini.Text = "Dosta, prekini";
+            this.bt_prekini.UseVisualStyleBackColor = true;
+            this.bt_prekini.Click += new System.EventHandler(this.bt_prekini_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 243);
+            this.Controls.Add(this.bt_prekini);
+            this.Controls.Add(this.bt_obrada);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button bt_obrada;
+        private System.Windows.Forms.Button bt_prekini;
+    }
+}
+
---
+++ WindowsService9v2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsService9v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct DataParameter
+        {
+            public int Process;
+            public int Delay;
+        }
+
+        private DataParameter _inputparameter;
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int process = ((DataParameter)e.Argument).Process;
+            int delay = ((DataParameter)e.Argument).Delay;
+            int index = 1;
+            try
+            {
+                for (int i = 0; i < process; i++)
+                {
+                    if (!backgroundWorker.CancellationPending)
+                    {
+                        backgroundWorker.ReportProgress(index++ * 100 / process,string.Format($"Process data {0},i"));
+                        Thread.Sleep(delay);//used to simulate lengh of operation
+                        //Add your code here
+                        //...
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                backgroundWorker.CancelAsync();
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            progressBar.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Process has been completed", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void bt_obrada_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                _inputparameter.Delay = 500;
+                _inputparameter.Process = 100;
+                backgroundWorker.RunWorkerAsync(_inputparameter);
+            }
+        }
+
+        private void bt_prekini_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                backgroundWorker.CancelAsync();
+            }
+        }
+    }
+}
